c9b8d115e0dfd3c9db516a5e0c1f24a6efe0878f
Facundo <facundomoya18@hotmail.com>
2025-01-10 23:37:58 -0300

    one more validation was added in the logout button to confirm the user's logout.

--- src/controllers/menuController.java
+++ src/controllers/menuController.java
@@ -1,4 +1,5 @@
 package controllers;
+import javax.swing.JOptionPane;
 import static models.dbConnection.showStudent;
 import views.menuView;
 
@@ -10,8 +11,17 @@ public class menuController {
     }
     
     public static void exitButton(){
+       int option = JOptionPane.showConfirmDialog(
+                null,                          // El componente padre (null para que sea una ventana emergente)
+                "Are you sure you want to logout?", // El mensaje a mostrar
+                "Logout confirmation",      // El título de la ventana
+                JOptionPane.YES_NO_OPTION,     // Los botones: "YES" y "NO"
+                JOptionPane.QUESTION_MESSAGE   // Tipo de mensaje (en este caso, una pregunta)
+        );
+    if(option == 0){
     loginController.view.setVisible(true);
     view.setVisible(false);
+    }
     }
     
     public static void studentClick(){
